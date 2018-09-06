cfab406295b35cad6817160f6523b2bd7ba0decc
Daniel Araujo Gomes <daniel.araujo@live.com>
2018-09-05 22:38:32 -0300

    Alteração ainda em andamento.

--- Logic/SimpleBotUser.cs
+++ Logic/SimpleBotUser.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using MongoDB.Bson;
+using MongoDB.Driver;
 
 namespace SimpleBot
 {
@@ -9,16 +11,54 @@ namespace SimpleBot
     {
         public static string Reply(Message message)
         {
+
+            //var client = new MongoClient("mongodb://localhost:27017");
+
+            //var doc = new BsonDocument()
+            //{
+            //    { "id", message.Id},
+            //    { "texto", message.Text },
+            //    { "app", "teste" },
+            //    { "campo", "ok"},
+            //    { "campo2", 2 },
+            //    { "campo3", new BsonDocument { { "campo2", 2 } } }
+            //};
+
+            //var db = client.GetDatabase("db01");
+            //var col = db.GetCollection<BsonDocument>("tabela01");
+            //col.InsertOne(doc);
+
+            var proFile = GetProfile(message.Id);
+
+            proFile.Visitas += 1;
+
+            SetProfile(message.Id, proFile);
+            
             return $"{message.User} disse '{message.Text}'";
         }
 
         public static UserProfile GetProfile(string id)
         {
-            return null;
+            var client = new MongoClient("mongodb://localhost:27017");
+
+            var db = client.GetDatabase("db01");
+            var col = db.GetCollection<UserProfile>("profile");
+
+            var filtro = Builders<UserProfile>.Filter.Eq("id", id);
+
+            return col.Find(filtro).First() ?? new UserProfile { Id = id , Visitas = 0 };
         }
 
         public static void SetProfile(string id, UserProfile profile)
         {
+            var client = new MongoClient("mongodb://localhost:27017");
+
+            var db = client.GetDatabase("db01");
+            var col = db.GetCollection<UserProfile>("profile");
+
+            var filtro = Builders<UserProfile>.Filter.Eq("id", id);
+
+            col.ReplaceOne(filtro, profile);
         }
     }
 }
